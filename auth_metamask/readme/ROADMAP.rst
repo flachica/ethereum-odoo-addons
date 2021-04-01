68e471af8f3b80e64ceb5821a6e8a26e79fd287a
Fernando La Chica <fernando.lachica@guadaltech.es>
2021-04-01 17:33:12 +0200

    [FIX] Some improve to remove WIP tag
    * Improve the method for login. It would be more appropriate to avoid the use of the login and password inputs
    * Use widget correctly
    * Avoid using the alert function on the client

--- auth_metamask/readme/ROADMAP.rst
+++ auth_metamask/readme/ROADMAP.rst
@@ -1,5 +1,4 @@
-* Improve the method for login. It would be more appropriate to avoid the use of the login and password inputs
-* Use widget correctly
-* Avoid using the alert function on the client
+* Improve translations
 * Correct pre-commit config to allow import web3 without eth_account.messages warning
 * Suggest to OCA this project
+* And much more . . .
--- auth_metamask/static/src/js/auth_metamask.js
+++ auth_metamask/static/src/js/auth_metamask.js
@@ -1,86 +1,144 @@
 odoo.define("auth_metamask.client", function(require) {
     "use strict";
 
-    require("web.dom_ready");
+    var publicWidget = require("web.public.widget");
+    var core = require("web.core");
+    var _t = core._t;
 
-    if (!$(".metamask-login").length) {
-        return Promise.reject("DOM doesn't contain '.js_survey_timer'");
-    }
+    publicWidget.registry.SignUpMetamaskForm = publicWidget.Widget.extend({
+        selector: ".oe_login_form",
+        events: {
+            "click .metamask-login": "_onMetamaskLoginClick",
+        },
+        start: function() {
+            var def = this._super.apply(this, arguments);
+            if (!$(".metamask-login").length) {
+                this.displayNotification({
+                    title: _t("Something went wrong."),
+                    message: _t("Template does not loaded."),
+                    type: "Error",
+                    sticky: true,
+                });
+            }
+            return def;
+        },
+        handleSignMessage: async function(web3, publicAddress, nonce) {
+            try {
+                const signature = await web3.eth.personal.sign(
+                    _t("Please, sign this nonce to allow your login: ") + nonce,
+                    publicAddress,
+                    ""
+                );
+                this.displayNotification({
+                    title: _t("Signed, waiting response."),
+                    message: _t(
+                        "You has signed your login info. Waiting response, be patient, please . . ."
+                    ),
+                    type: "Info",
+                    sticky: true,
+                });
+                return {publicAddress, signature};
+            } catch (err) {
+                this.displayNotification({
+                    title: _t("Something went wrong."),
+                    message:
+                        _t(
+                            "You need to sign the message to be able to log in. Error getted "
+                        ) + err,
+                    type: "Error",
+                    sticky: true,
+                });
+            }
+        },
+        _onMetamaskLoginClick: async function() {
+            var self = this;
+            if (
+                typeof window.web3 !== "undefined" &&
+                typeof window.web3.currentProvider !== "undefined"
+            ) {
+                // eslint-disable-next-line
+                var web3 = new Web3(window.web3.currentProvider);
+            } else {
+                // eslint-disable-next-line
+                var web3 = new Web3();
+            }
 
-    async function handleSignMessage(web3, publicAddress, nonce) {
-        try {
-            const signature = await web3.eth.personal.sign(
-                "Please, sign this nonce to allow your login: " + nonce,
-                publicAddress,
-                ""
-            );
+            if (!window.ethereum) {
+                this.displayNotification({
+                    title: _t("Something went wrong."),
+                    message: _t("Please install MetaMask first."),
+                    type: "Error",
+                    sticky: true,
+                });
+                return;
+            }
 
-            return {publicAddress, signature};
-        } catch (err) {
-            window.alert(
-                "You need to sign the message to be able to log in. Error getted " + err
-            );
-        }
-    }
+            try {
+                // Request account access if needed
+                await window.ethereum.enable();
 
-    async function handleClick() {
-        if (
-            typeof window.web3 !== "undefined" &&
-            typeof window.web3.currentProvider !== "undefined"
-        ) {
-            // eslint-disable-next-line
-            var web3 = new Web3(window.web3.currentProvider);
-        } else {
-            // eslint-disable-next-line
-            var web3 = new Web3();
-        }
+                // eslint-disable-next-line
+                web3 = new Web3(window.ethereum);
+            } catch (error) {
+                this.displayNotification({
+                    title: _t("Something went wrong."),
+                    message: _t("You need to allow MetaMask."),
+                    type: "Error",
+                    sticky: true,
+                });
+                return;
+            }
 
-        if (!window.ethereum) {
-            window.alert("Please install MetaMask first.");
-            return;
-        }
+            const coinbase = await web3.eth.getCoinbase();
+            if (!coinbase) {
+                this.displayNotification({
+                    title: _t("Something went wrong."),
+                    message: _t("Please activate MetaMask first."),
+                    type: "Error",
+                    sticky: true,
+                });
+                return;
+            }
 
-        try {
-            // Request account access if needed
-            await window.ethereum.enable();
-
-            // eslint-disable-next-line
-            web3 = new Web3(window.ethereum);
-        } catch (error) {
-            window.alert("You need to allow MetaMask.");
-            return;
-        }
-
-        const coinbase = await web3.eth.getCoinbase();
-        if (!coinbase) {
-            window.alert("Please activate MetaMask first.");
-            return;
-        }
-
-        const publicAddress = coinbase.toLowerCase();
-        $.get("/metamask/" + publicAddress, {})
-            .then(
-                function(adr_nonce) {
-                    var jsonResult = JSON.parse(adr_nonce);
-                    return handleSignMessage(
-                        web3,
-                        jsonResult.public_address,
-                        jsonResult.nonce
-                    );
-                },
-                function(error) {
-                    alert(error);
-                }
-            )
-            .then(function(signed) {
-                $("form").attr("autocomplete", "off");
-                $("#login").attr("autocomplete", "off");
-                $("#password").attr("autocomplete", "off");
-                $("#login").val(signed.publicAddress);
-                $("#password").val(signed.signature);
-                $("button[type=submit]")[0].click();
-            });
-    }
-
-    $(".metamask-login").click(handleClick);
+            const publicAddress = coinbase.toLowerCase();
+            $.get("/metamask/" + publicAddress, {})
+                .then(
+                    function(adr_nonce) {
+                        var jsonResult = JSON.parse(adr_nonce);
+                        return self.handleSignMessage(
+                            web3,
+                            jsonResult.public_address,
+                            jsonResult.nonce
+                        );
+                    },
+                    function(error) {
+                        this.displayNotification({
+                            title: _t("Something went wrong."),
+                            message: error,
+                            type: "Error",
+                            sticky: true,
+                        });
+                    }
+                )
+                .then(function(signed) {
+                    var data = {
+                        login: signed.publicAddress,
+                        password: signed.signature,
+                        csrf_token: $("[name='csrf_token']").val(),
+                    };
+                    $.post($("form").action, data)
+                        .then(function() {
+                            window.location = "/web";
+                        })
+                        .fail(function(response) {
+                            this.displayNotification({
+                                title: _t("Something went wrong."),
+                                message: response.responseText,
+                                type: "Error",
+                                sticky: true,
+                            });
+                        });
+                });
+        },
+    });
 });
